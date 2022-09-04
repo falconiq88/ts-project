dad0656fc1b5cdf65996e4657892b0fda8d24dc0
mohammed alkilani <58834459+falconiq88@users.noreply.github.com>
2022-09-04 21:28:26 +0300

    main

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-# sudo chmod -R 777 /home/ubuntu/node-app/src
+sudo chmod -R 777 /home/ubuntu/node-app/
 
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/ts-app/
